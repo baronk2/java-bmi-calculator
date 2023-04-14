9b748d9a19179416800373a2ced167aa1dc5a207
Kevin Baron <kevinbaron14@comcast.net>
2023-04-14 02:22:47 -0700

    Init

---
+++ KWBaron_BMI_Calculator.java
@@ -0,0 +1,20 @@
+/*
+Kevin Baron
+11/21/12
+BMI calculator
+*/
+
+import java.util.*;
+
+public class KWBaron_BMI_Calculator {
+
+	public static void main(String[] args) {
+		intro();
+		
+	}//eomain
+
+	public static void intro() {
+		System.out.println("This program compares the BMIs of two\npeople given the height and weight of each.");
+	}//eointro
+
+}//eoclass
